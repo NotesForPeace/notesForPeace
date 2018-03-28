9aa8563146c111d32168e78e4cc540381a1690ea
Ben Callanta <callanta@gmail.com>
2018-03-28 17:39:03 -0500

    Add page title and content.

--- page-tributes.php
+++ page-tributes.php
@@ -13,6 +13,27 @@
 
         <?php get_header(); ?>
 
+		<!-- Title and Content of the Tributes Page -->
+		<?php if ( have_posts() ) : ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+                
+                <div class="container py-4">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-12">
+                         <h1><?php the_title(); ?></h1>   
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 col-lg-12">
+                            <p><?php the_content(); ?></p>
+                        </div>
+                    </div>
+                </div>
+                
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+		<!-- Grid of Tribute Cards -->
 		<div class="container mt-3">
 			<div class="row">
 				<div class="card-deck">
@@ -20,7 +41,7 @@
 							$posts = get_posts(array(
 								'meta_key' => 'Last Name',
 								'orderby'  => 'meta_value',
-								'order'	   => 'ASC'
+								'order'	   => 'DESC'
 							));
 
 							foreach ($posts as $post) {
@@ -30,16 +51,17 @@
 								$tributeFirstName = get_post_meta($tributeID, 'First Name', true);
 								$tributeLastName  = get_post_meta($tributeID, 'Last Name', true);
 								$tributeSummary   = get_post_meta($tributeID, 'Summary', true);
-								$tributeImageURL  = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+								$tributeImageURL  = wp_get_attachment_image_src( get_post_thumbnail_id( $tributeID ), 'single-post-thumbnail' );
 
-								echo '<div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">';
-									echo '<img class="card-img-top" src="'.$tributeImageURL[0].'" alt="Card image cap">';
-									echo '<h5 class="card-title">'.$tributeTitle.'</h5>';
-									echo '<div class="card-body">';
-										echo '<p class="card-text">'.$tributeSummary.'</p>';
-										echo '<p class="card-text"><small class="text-muted"><a href="'.$tributeURL.'">learn more</a></small></p>';
+								echo '<a href="'.$tributeURL.'">';
+									echo '<div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">';
+										echo '<img class="card-img-top" src="'.$tributeImageURL[0].'" alt="Card image cap">';
+										echo '<h5 class="card-title">'.$tributeTitle.'</h5>';
+										echo '<div class="card-body">';
+											echo '<p class="card-text">'.$tributeSummary.'</p>';
+										echo '</div>';
 									echo '</div>';
-								echo '</div>';
+								echo '</a>';
 
 							}
 						?>
