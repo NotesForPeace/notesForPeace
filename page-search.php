a779e5f998cb6a3fb54e8631bea9888233923a4d
Ben Callanta <callanta@gmail.com>
2018-03-27 21:43:46 -0500

    Add the title and content of the search page.

--- page-search.php
+++ page-search.php
@@ -12,6 +12,26 @@
 ?>
 
         <?php get_header(); ?>
+
+        <?php if ( have_posts() ) : ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+                
+                <div class="container py-4">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-12">
+                         <h1><?php the_title(); ?></h1>   
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 col-lg-12">
+                            <p><?php the_content(); ?></p>
+                        </div>
+                    </div>
+                </div>
+
+                
+            <?php endwhile; ?>
+        <?php endif; ?>
         
         <?php get_footer(); ?>
 
