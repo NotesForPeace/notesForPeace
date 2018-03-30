237341fb524fa50c5563a8f89caf85e3bc499b7d
Ben Callanta <callanta@gmail.com>
2018-03-30 10:19:40 -0500

    Adjust tribute card layout.

--- page-tributes.php
+++ page-tributes.php
@@ -17,7 +17,7 @@
 		<?php if ( have_posts() ) : ?>
             <?php while ( have_posts() ) : the_post(); ?>
                 
-                <div class="container py-4">
+                <div class="container-fluid p-5">
                     <div class="row">
                         <div class="col-md-6 col-lg-12">
                          <h1><?php the_title(); ?></h1>   
@@ -34,9 +34,9 @@
         <?php endif; ?>
 
 		<!-- Grid of Tribute Cards -->
-		<div class="container mt-3">
+		<div class="container-fluid px-5 mt-4 myContainer">
 			<div class="row">
-				<div class="card-deck">
+				<div class="card-deck justify-content-center">
 						<?php 
 							$args = array(
 								'posts_per_page'   	=> -1,
@@ -69,7 +69,7 @@
 								$tributeImageURL  = wp_get_attachment_image_src( get_post_thumbnail_id( $tributeID ), 'single-post-thumbnail' );
 
 								echo '<a href="'.$tributeURL.'">';
-									echo '<div class="card mb-4" style="min-width: 18rem; max-width: 18rem;">';
+									echo '<div class="card ml-3 mb-4" style="min-width: 250px; max-width: 250px;">';
 										echo '<img class="card-img-top" src="'.$tributeImageURL[0].'" alt="Card image cap">';
 										echo '<h5 class="card-title">'.$tributeTitle.'</h5>';
 										echo '<div class="card-body">';
