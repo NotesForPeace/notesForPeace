6d3dfc57763c14bbfeff3ce3598f12605bdd173f
Ben Callanta <callanta@gmail.com>
2018-03-30 17:03:09 -0500

    Add sidebar support for the theme.

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
-    //-------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------
     //Load the Bootstrap files, Google Fonts, and stylesheet for the NFP Theme
+    //---------------------------------------------------------------------------------------------
     function enqueue_nfp_scripts() {
         wp_enqueue_script('bootstrap-js', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js', array('jquery'));
         wp_enqueue_style('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
@@ -10,8 +11,9 @@
     add_action('wp_enqueue_scripts', 'enqueue_nfp_scripts');
 
 
-    //-------------------------------------------------------------------------
-    //Initialize the NFP Theme
+    //---------------------------------------------------------------------------------------------
+    // Initialize the NFP Theme
+    //---------------------------------------------------------------------------------------------
     function nfp_setup() {
 
         //Gather all the links in the custom menu created in wp-admin
@@ -37,8 +39,29 @@
     }
     add_action('after_setup_theme', 'nfp_setup' );
 
-    
-    //-------------------------------------------------------------------------
-    //Utility that bridges custom menus from Wordpress with Bootstrap 4
+
+    //---------------------------------------------------------------------------------------------
+    // Wordpress has a built-in feature called widgets where we can add functionality like 
+    // displaying a list of page links in our site, or having a live chat box inside of the 
+    //sidebar. First, we have to "register" the sidebar so that Wordpress is aware of it and 
+    // ready to use it.
+    //---------------------------------------------------------------------------------------------
+    function nfp_widgets() {
+        register_sidebar( array(
+            'name'          => __( 'Sidebar', 'customtheme' ),
+            'id'            => 'sidebar-1',
+            'description'   => __( 'Add widgets here to appear in the sidebar', 'nfp' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>'
+        ) );
+    }
+    add_action( 'widgets_init', 'nfp_widgets' );
+
+
+    //---------------------------------------------------------------------------------------------
+    // Utility that bridges custom menus from Wordpress with Bootstrap 4
+    //---------------------------------------------------------------------------------------------
     require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 ?>
