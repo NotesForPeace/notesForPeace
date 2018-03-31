a5991b8643555ddb6ec747d305e693e29a7b87d7
Ben Callanta <callanta@gmail.com>
2018-03-31 10:49:01 -0500

    Add faceted search to filter tributes.

--- page-tributes.php
+++ page-tributes.php
@@ -31,57 +31,55 @@
                 </div>
                 
             <?php endwhile; ?>
-        <?php endif; ?>
+		<?php endif; ?>
+		
+
 
 		<!-- Grid of Tribute Cards -->
 		<div class="container-fluid px-5 mt-4 myContainer">
 			<div class="row">
-				<div class="card-deck justify-content-center">
-						<?php 
-							$args = array(
-								'posts_per_page'   	=> -1,
-								'offset'           	=> 0,
-								'category'         	=> '',
-								'category_name'    	=> '',
-								'orderby'          	=> 'meta_value',
-								'order'            	=> 'ASC',
-								'include'          	=> '',
-								'exclude'          	=> '',
-								'meta_key'         	=> 'Last Name',
-								'meta_value'       	=> '',
-								'post_type'        	=> 'post',
-								'post_mime_type'   	=> '',
-								'post_parent'      	=> '',
-								'author'			=> '',
-								'author_name'	   	=> '',
-								'post_status'      	=> 'publish',
-								'suppress_filters' 	=> true 
-							);
-							$tributes = get_posts($args);
+				<div class="col-lg-3">
+					<!-- Faceted Search -->
+					<?php get_sidebar(); ?> 
+				</div>
+				<div class="col-lg-9">
+					<div class="card-deck justify-content-center facetwp-template">
+							<?php 
+								$args = array(
+									'posts_per_page' => -1, 			//-1 to return all posts
+									'orderby'        => 'meta_value', 	//Meta values and custom fields are the same thing
+									'order'          => 'ASC', 			//Ascending, use DESC for descending
+									'meta_key'       => 'Last Name',    //TODO: We should move all the custom field key names into some centralized location
+									'post_type'      => 'post',			//TODO: We need to finalize our data model
+									'post_status'    => 'publish',		//Get only the posts that are meant to be public
+									'facetwp'        => true 			//Tell FacetWP that these are the posts to filter
+								);
+								$tributes = get_posts($args);
 
-							foreach ($tributes as $tribute) {
-								$tributeID 	 	  = $tribute->ID;
-								$tributeTitle     = apply_filters( 'the_title', $tribute->post_title);
-								$tributeURL       = get_permalink($tributeID);
-								$tributeFirstName = get_post_meta($tributeID, 'First Name', true);
-								$tributeLastName  = get_post_meta($tributeID, 'Last Name', true);
-								$tributeSummary   = get_post_meta($tributeID, 'Summary', true);
-								$tributeImageURL  = wp_get_attachment_image_src( get_post_thumbnail_id( $tributeID ), 'single-post-thumbnail' );
+								foreach ($tributes as $tribute) {
+									$tributeID 	 	  = $tribute->ID;
+									$tributeTitle     = apply_filters( 'the_title', $tribute->post_title);
+									$tributeURL       = get_permalink($tributeID);
+									$tributeFirstName = get_post_meta($tributeID, 'First Name', true);
+									$tributeLastName  = get_post_meta($tributeID, 'Last Name', true);
+									$tributeSummary   = get_post_meta($tributeID, 'Summary', true);
+									$tributeImageURL  = wp_get_attachment_image_src( get_post_thumbnail_id( $tributeID ), 'single-post-thumbnail' );
 
-								echo '<a href="'.$tributeURL.'">';
-									echo '<div class="card ml-3 mb-4" style="min-width: 250px; max-width: 250px;">';
-										echo '<img class="card-img-top" src="'.$tributeImageURL[0].'" alt="Card image cap">';
-										echo '<h5 class="card-title">'.$tributeTitle.'</h5>';
-										echo '<div class="card-body">';
-											echo '<p class="card-text">'.$tributeSummary.'</p>';
+									echo '<a href="'.$tributeURL.'">';
+										echo '<div class="card ml-3 mb-4" style="min-width: 250px; max-width: 250px;">';
+											echo '<img class="card-img-top" src="'.$tributeImageURL[0].'" alt="Card image cap">';
+											echo '<h5 class="card-title">'.$tributeTitle.'</h5>';
+											echo '<div class="card-body">';
+												echo '<p class="card-text">'.$tributeSummary.'</p>';
+											echo '</div>';
 										echo '</div>';
-									echo '</div>';
-								echo '</a>';
-
-							}
-						?>
+									echo '</a>';
+								} //foreach
+							?>
+					</div>
 				</div>
 			</div>
 		</div>
+
 
         <?php get_footer(); ?>
