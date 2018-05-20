f32a855c6d4918204f55b41b24d59e7708bcabd5
ljones37 <lindseyjones2017@u.northwestern.edu>
2018-05-20 17:16:42 -0500

    only pull over images, add margins

--- single.php
+++ single.php
@@ -81,9 +81,10 @@
                 <div class="tab-pane fade show active media" id="pills-about" role="tabpanel" aria-labelledby="pills-about-tab">
                     <div class="media-body about-individual">
                         <div class="row">
-                            <div class="col-8" id="about-text"></div>
-                            <div class="col-4" id="about-images"></div>
-                            <?php echo get_field('about_individual') ?>
+                            <div class="col-sm-8">
+                                <?php echo get_field('about_individual') ?>
+                            </div>
+                            <div class="col-sm-4" id="about-images"></div>
                         </div>
                     </div>
                 </div>
