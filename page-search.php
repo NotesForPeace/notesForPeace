e610dc9d736dd8bfa2784612564cc5206cea2891
dgjr <33696585+dgjr@users.noreply.github.com>
2018-05-22 21:47:31 -0500

    add ajaxcontent tag

--- page-search.php
+++ page-search.php
@@ -20,43 +20,42 @@
     add_action('wp_enqueue_scripts', 'enqueue_search_scripts');
 
 ?>
-        <?php get_header(); ?>
-        
-        <!-- Search Page Header -->
-		<?php if ( have_posts() ) : ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				
-				<div class="container pt-4">
-					<div class="row justify-content-center">
-						<div class="col">
-							<h1 class="text-center"><?php the_title(); ?></h1>   
-						</div>
+<?php get_header(); ?>
+<div id="ajaxcontent">
+	<!-- Search Page Header -->
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<div class="container pt-4">
+				<div class="row justify-content-center">
+					<div class="col">
+						<h1 class="text-center"><?php the_title(); ?></h1>   
 					</div>
 				</div>
-				
-			<?php endwhile; ?>
-		<?php endif; ?>
+			</div>
 
-        <?php $search = new WP_Advanced_Search('tributeSearch');?>
+		<?php endwhile; ?>
+	<?php endif; ?>
 
-        <div class="container">
-            <div class="row justify-content-center">
-            <div id="facetedSearch" class="col-9">
-                    <?php $search->the_form(); ?>
-                </div>
-            </div>
+	<?php $search = new WP_Advanced_Search('tributeSearch');?>
 
-            <div class="row">
+	<div class="container">
+	    <div class="row justify-content-center">
+	    <div id="facetedSearch" class="col-9">
+		    <?php $search->the_form(); ?>
+		</div>
+	    </div>
 
-
-
-                <div class="col-12">
-                    <!-- This is results will be loaded -->
-                    <div id="wpas-results"></div> 
-                </div>
-
-            </div>
-        </div>
+	    <div class="row">
 
 
-        <?php get_footer(); ?>
+
+		<div class="col-12">
+		    <!-- This is results will be loaded -->
+		    <div id="wpas-results"></div> 
+		</div>
+
+	    </div>
+	</div>
+</div>
+<?php get_footer(); ?>
